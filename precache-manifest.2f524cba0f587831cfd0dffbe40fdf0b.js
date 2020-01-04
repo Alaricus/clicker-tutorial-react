a457ca48873ffaffab2c7178067e4bcb36265344
Alaric von Teplitz <alaric2000@gmail.com>
2020-01-03 21:48:33 -0800

    Updates

---
+++ precache-manifest.2f524cba0f587831cfd0dffbe40fdf0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29b86aba6b2c49e3f246f23e33c5b35b",
+    "url": "/clicker-tutorial/index.html"
+  },
+  {
+    "revision": "f8882458fc6111ceae78",
+    "url": "/clicker-tutorial/static/css/main.8db9501f.chunk.css"
+  },
+  {
+    "revision": "93740a58f7524968eeab",
+    "url": "/clicker-tutorial/static/js/2.7f067dca.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicker-tutorial/static/js/2.7f067dca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f8882458fc6111ceae78",
+    "url": "/clicker-tutorial/static/js/main.7da3a4f9.chunk.js"
+  },
+  {
+    "revision": "b715538f3d976694b517",
+    "url": "/clicker-tutorial/static/js/runtime-main.413ca7e5.js"
+  }
+]);
