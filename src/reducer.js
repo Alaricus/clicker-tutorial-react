e86716e5749c2c5fd3161f825f5b89d68d7e3f30
Alaric von Teplitz <alaric2000@gmail.com>
2020-07-26 13:59:16 -0700

    Address negative values when buying and selling

--- src/reducer.js
+++ src/reducer.js
@@ -12,17 +12,23 @@ const reducer = (state, action) => {
     case 'click':
       return { ...state, clicks: { ...state.clicks, amount: state.clicks.amount + 1 } };
     case 'increase':
-      return {
-        ...state,
-        [action.tier]: { ...state[action.tier], amount: state[action.tier].amount + 1 },
-        clicks: { amount: state.clicks.amount - state[action.tier].cost },
-      };
+      if (state[action.tier].cost <= state.clicks.amount) {
+        return {
+          ...state,
+          [action.tier]: { ...state[action.tier], amount: state[action.tier].amount + 1 },
+          clicks: { amount: state.clicks.amount - state[action.tier].cost },
+        };
+      }
+      return state;
     case 'decrease':
-      return {
-        ...state,
-        [action.tier]: { ...state[action.tier], amount: state[action.tier].amount - 1 },
-        clicks: { amount: state.clicks.amount + state[action.tier].cost },
-      };
+      if (state[action.tier].amount > 0) {
+        return {
+          ...state,
+          [action.tier]: { ...state[action.tier], amount: state[action.tier].amount - 1 },
+          clicks: { amount: state.clicks.amount + state[action.tier].cost },
+        };
+      }
+      return state;
     case 'update':
       return (action.payload === state.clicks.amount)
         ? state
